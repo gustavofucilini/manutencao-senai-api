2cc5233057f5723861f4da4fe1d6c28396c261de
Gustavo Fucilini <gustavofucilini@gmail.com>
2022-04-07 21:13:39 -0300

    Arrumar erros

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,6 +1,5 @@
 package br.com.senai.manutencaosenaiapi;
 
-import org.springframework.beans.factory.wiring.BeanWiringInfo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
